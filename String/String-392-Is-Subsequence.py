ef805d28a2dd8d919576bf0d68f2ef07263d2833
alp <alpkarar0@gmail.com>
2024-05-06 14:05:44 +0300

    Is Subsequence - 91.71%

---
+++ String/String-392-Is-Subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        j = 0
+
+        while i < len(s):
+            while j < len(t) and s[i] != t[j]:
+                j += 1
+
+            if j == len(t):
+                return False
+            
+            i += 1
+            j += 1
+        
+        return True
