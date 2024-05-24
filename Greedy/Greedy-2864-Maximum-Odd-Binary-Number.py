c73e3d457163099936d9ec58aff4c575ae081f92
alp <alpkarar0@gmail.com>
2024-05-24 17:32:34 +0300

    Maximum Odd Binary Number - Soln2 - 88.26%

--- Greedy/Greedy-2864-Maximum-Odd-Binary-Number.py
+++ Greedy/Greedy-2864-Maximum-Odd-Binary-Number.py
@@ -1,18 +1,9 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        sorted_s = ""
-        isFirst = True
+        count_one_bit = 0
 
         for bit in s:
             if bit == "1":
-                if isFirst:
-                    isFirst = False
-                else:
-                    sorted_s = bit + sorted_s
-            else:
-                sorted_s += bit
+                count_one_bit += 1
         
-        if not isFirst:
-            sorted_s += "1"
-        
-        return sorted_s
+        return (count_one_bit - 1) * "1" + (len(s) - count_one_bit) * "0" + "1"
