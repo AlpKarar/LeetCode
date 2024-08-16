9902f91a4cc4f5ec51cf7ef61e7622ff9277cd26
alp <alpkarar0@gmail.com>
2024-08-16 22:35:32 +0300

    Number of Arithmetic Triplets - 60.83% - Time: O(n) & Space: O(n)

---
+++ Array/Array-2367-Number-of-Arithmetic-Triplets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        nums_set = set(nums)
+        res = 0
+
+        for num in nums:
+            if num + diff in nums_set and num + 2 * diff in nums_set:
+                res += 1
+        
+        return res
