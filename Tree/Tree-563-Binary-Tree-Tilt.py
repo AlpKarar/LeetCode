9ab7a0b2b5ffb0b5e1a1d7219887495605dc2014
alp <alpkarar0@gmail.com>
2024-09-04 22:39:09 +0300

    Binary Tree Tilt - 91.60% - Time: O(n) & Space: O(n)

---
+++ Tree/Tree-563-Binary-Tree-Tilt.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        tilts = []
+        
+        def helper(root):
+            if not root:
+                return 0
+            
+            left = helper(root.left)
+            right = helper(root.right)
+
+            tilts.append(abs(left - right))
+
+            return left + right + root.val
+        
+        helper(root)
+
+        return sum(tilts)
